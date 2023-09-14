06fafebbc625498a21c4fc4de4a295177b3d6169
Andrew Monteith <339531824@GoTVDSB.ca>
2023-09-14 11:01:34 -0400

    Errors Completed

--- Program.cs
+++ Program.cs
@@ -10,6 +10,40 @@ namespace Part_5_Console_Decisions
     {
         static void Main(string[] args)
         {
+            Random generator = new Random();
+            double balance = generator.Next(100, 501);
+            balance += generator.NextDouble();
+            int transaction;
+            Console.WriteLine("Simple Banking Machine");
+            Console.WriteLine("Your Current Balance Is: "+ balance.ToString("C"));
+            Console.WriteLine("Enter a 1 for Deposit, 2 for Withdrawal, or 3 for bill payment");
+            if (int.TryParse(Console.ReadLine(), out transaction))
+            {
+                if (transaction == 1)
+                {
+
+                }
+                else if (transaction == 2)
+                {
+
+                }
+                else if (transaction == 3)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Transaction");
+                    Console.WriteLine("Your New Balance is " + (balance - 0.75).ToString("C"));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Transaction");
+                Console.WriteLine("Your New Balance is " + (balance-0.75).ToString("C"));
+            }
+
+            Console.ReadLine();
         }
     }
 }
